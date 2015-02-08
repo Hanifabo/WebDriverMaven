465a49fad9ed1ec13190a689ced2a4eb757aaaf9
Hanifabo <hanifabo@gmail.com>
2015-02-08 14:43:15 -0800

    Addin to the new ones

---
+++ WebDriverMaven/WebDriverMaven/run_local.sh
@@ -0,0 +1,2 @@
+bundle exec rake spec:browser suite=test  type=local
+Hanifa is adding this to see how it appears on git
---
+++ WebDriverMaven/WebDriverMaven/sauce_env.sh
@@ -0,0 +1,3 @@
+export SAUCE_USERNAME='ctodev_livecommunity'
+export SAUCE_ACCESS_KEY='952b4218-9ef5-4cc9-b7ee-b728c73dfb4e'
+this is the second one i want to check
