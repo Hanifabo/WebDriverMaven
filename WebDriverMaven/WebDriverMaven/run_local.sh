f097cc152858f44e568afdb1ae85ec7d5370053d
Hanifabo <hanifabo@gmail.com>
2015-02-08 14:48:57 -0800

    Adding the new Changes

--- WebDriverMaven/WebDriverMaven/run_local.sh
+++ WebDriverMaven/WebDriverMaven/run_local.sh
@@ -1,2 +1,4 @@
 bundle exec rake spec:browser suite=test  type=local
 Hanifa is adding this to see how it appears on git
+what is going on here, why am i not seeing the files
+is something the matter
--- WebDriverMaven/WebDriverMaven/sauce_env.sh
+++ WebDriverMaven/WebDriverMaven/sauce_env.sh
@@ -1,3 +1,4 @@
 export SAUCE_USERNAME='ctodev_livecommunity'
 export SAUCE_ACCESS_KEY='952b4218-9ef5-4cc9-b7ee-b728c73dfb4e'
 this is the second one i want to check
+this is still not running the way i wanted it to run
