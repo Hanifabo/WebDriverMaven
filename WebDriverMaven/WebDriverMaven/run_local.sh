52db466edee5913cbb79ecb794ee40c496d7c62a
Hanifabo <hanifabo@gmail.com>
2015-02-08 16:11:37 -0800

    Addine new sutff

--- WebDriverMaven/WebDriverMaven/run_local.sh
+++ WebDriverMaven/WebDriverMaven/run_local.sh
@@ -2,3 +2,6 @@ bundle exec rake spec:browser suite=test  type=local
 Hanifa is adding this to see how it appears on git
 what is going on here, why am i not seeing the files
 is something the matter
+The midfield battle between experienced Yaya and young Mubarak was always exciting with the younger getting the edge on many occasions.
+Wakaso Mubarak struck from 30 yards in the 67th minute but the ball inched just wide.
+- See more at: http://www.myjoyonline.com/sports/2015/February-8th/ivory-coast-break-jinx-ghana-hearts-to-win-afcon-trophy.php#sthash.5BRrBW1c.dpuf
--- WebDriverMaven/WebDriverMaven/sauce_env.sh
+++ WebDriverMaven/WebDriverMaven/sauce_env.sh
@@ -2,3 +2,6 @@ export SAUCE_USERNAME='ctodev_livecommunity'
 export SAUCE_ACCESS_KEY='952b4218-9ef5-4cc9-b7ee-b728c73dfb4e'
 this is the second one i want to check
 this is still not running the way i wanted it to run
+The game inched close to extra-time but it was the Ghanaians who lived on tight rope in added time as Aurier raced past Ayew with a lightening pace before whipping in an incisive cross which was parried back into play by Razak Braimah but there was man in orange to connect.
+ In extra time it was the Ghanaians who looked like scoring with Atsu controlling a free-kick on his chest with such impudence before advertising a half volley mid-air which whisked just wide.
+- See more at: http://www.myjoyonline.com/sports/2015/February-8th/ivory-coast-break-jinx-ghana-hearts-to-win-afcon-trophy.php#sthash.5BRrBW1c.dpuf
